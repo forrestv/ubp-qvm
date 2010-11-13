ca2b3fc5da3dc93e1e833a046edda4b000154fc1
forrest <forrest@470744a7-cac9-478e-843e-5ec1b25c69e8>
2010-11-13 20:59:41 +0000

    added uploader
    git-svn-id: svn://forre.st/ubp@1172 470744a7-cac9-478e-843e-5ec1b25c69e8

---
+++ upload.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd $(dirname $0)
+wput -u --basename build/release-linux-x86_64/ build/release-linux-x86_64/base/vm/game.qvm ftp://adeya:$(cat ~/.ubp_ftp)@server2.noghost.net/tremulous/
